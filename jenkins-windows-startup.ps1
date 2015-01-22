da1427f1fbe94905c941f185d4d44212e837b627
Matt Mitchell <mmitche@microsoft.com>
2015-01-22 15:41:53 -0800

    Alter casing of computer name.
    Jenkins created the JNLP name as the VM name, which is not the same as the computer name (casing-wise).
    We know that the name will take the form: Azure<Number>, so change AZURE to Azure.

--- jenkins-windows-startup.ps1
+++ jenkins-windows-startup.ps1
@@ -4,17 +4,25 @@ Set-ExecutionPolicy Unrestricted -force
 # If your jenkins server is configured for security, make sure to edit command for how slave executes
 # You may need to pass credentails or secret in the command , Refer to help by running "java -jar slave.jar --help" 
 
-$jenkinsserverurl = "http://corefx-ci.cloudapp.net/jenkins"
+$jenkinsserverurl = "http://corefx-ci.cloudapp.net/jenkins/"
 $vmname = $env:COMPUTERNAME
 
+# WORKAROUND - VM names are always 'Azure' and the computer name comes out as "AZURE".  Alter to avoid a 404 on the jnlp call.
+$vmname = $vmname.Substring(0,1) + $vmName.Substring(1).ToLower();
+
+# Download slave jar
+
+Write-Output "Downloading jenkins slave jar "
+$jarSource = $jenkinsserverurl + "jnlpJars/slave.jar"
+$jarDest = "$env:USERPROFILE\slave.jar"
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($jarSource, $jarDest)
+
 # execute slave
-Write-Output "Executing slave process "
-$java="java.exe"
-$jar="-jar"
-$jnlpUrl="-jnlpUrl" 
-$serverURL=$jenkinsserverurl+"computer/" + $vmname + "/slave-agent.jnlp"
-$secret="-jnlpCredentials"
+$serverURL=$jenkinsserverurl + "computer/" + $vmname + "/slave-agent.jnlp"
 # syntax for credentials username:apitoken or username:password
 # you can get api token by clicking on your username --> configure --> show api token
 $token="dotnet-bot:ca028e8f4a93ac3406bb47fd88ff80d3"
-& $java $jar $destSource $jnlpUrl $serverURL $secret $token
+$commandLine="java -jar $jarDest -jnlpUrl $serverURL -jnlpCredentials $token"
+Write-Output "Executing slave process: $commandLine"
+& java -jar $jarDest -jnlpUrl $serverURL -jnlpCredentials $token
