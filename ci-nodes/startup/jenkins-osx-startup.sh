120d2866f207f3c36985ec32b2508e5458bff9f5
Matt Mitchell <mmitche@microsoft.com>
2015-10-13 13:52:01 -0700

    Add /usr/local/bin path

--- ci-nodes/startup/jenkins-osx-startup.sh
+++ ci-nodes/startup/jenkins-osx-startup.sh
@@ -1,3 +1,5 @@
+# Reset the path because /local/bin doesn't get included
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 HOSTNAME=$(scutil --get LocalHostName)
 JENKINS_MASTER=$1
 JENKINS_CREDENTIALS=$2
