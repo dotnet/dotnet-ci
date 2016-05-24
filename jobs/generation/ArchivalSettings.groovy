484ab6ad96ebefc11210c15ac983c6347c2ed1e5
Matt Mitchell <mmitche@microsoft.com>
2016-05-24 15:54:29 -0700

    Move archival settings to another file

---
+++ jobs/generation/ArchivalSettings.groovy
@@ -0,0 +1,51 @@
+package jobs.generation;
+
+class ArchivalSettings {
+    String[] filesToArchive = null;
+    String[] filesToExclude = null;
+    boolean failIfNothingArchived = true
+    private String[] archiveStatus = defaultSuccessArchiveStatus
+    private static String[] defaultSuccessArchiveStatus = ['SUCCESS', 'SUCCESS']
+    private static String[] defaultFailingArchiveStatus = ['ABORTED', 'FAILURE']
+    private static String[] defaultAlwaysArchiveStatus = ['FAILURE', 'SUCCESS']
+    
+    void setArchiveOnFailure() {
+        archiveStatus = defaultFailingArchiveStatus
+    }
+    
+    void setArchiveOnSuccess() {
+        archiveStatus = defaultSuccessArchiveStatus
+    }
+    
+    void setAlwaysArchive() {
+        archiveStatus = defaultAlwaysArchiveStatus
+    }
+    
+    String[] getArchiveStatusRange() {
+        return archiveStatus
+    }
+    
+    void addFiles(String archiveBlob) {
+        if (filesToArchive == null) {
+            filesToArchive = [archiveBlob]
+        } else {
+            filesToArchive += archiveBlob
+        }
+    }
+    
+    void excludeFiles(String archiveBlob) {
+        if (filesToExclude == null) {
+            filesToExclude = [archiveBlob]
+        } else {
+            filesToExclude += archiveBlob
+        }
+    }
+    
+    boolean setFailIfNothingArchived() {
+        failIfNothingArchived = true
+    }
+    
+    boolean setDoNotFailIfNothingArchived() {
+        failIfNothingArchived = false
+    }
+}
