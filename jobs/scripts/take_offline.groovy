29a3df1296050849551004c6b95666a922a7edb9
Smile Wei <xiwe@microsoft.com>
2016-07-22 18:09:22 -0700

    Fix the script to take current machine offline.

--- jobs/scripts/take_offline.groovy
+++ jobs/scripts/take_offline.groovy
@@ -1,26 +1,13 @@
 import hudson.model.*
 import hudson.slaves.OfflineCause;
-  
-for (node in Hudson.getInstance().getNodes())
-{
-  println("Checking " + node.name);
-  def computer = node.getComputer()
-  if (computer.isIdle())
-  {
-    println("  Attempting to take offline. " + node.name);
-    if (node.getComputer().isOffline()) {
-      println("  Offline, skipping");
-      continue
-    }
-    
+
+def node = Hudson.getInstance().getNode($NODE_NAME))
+println("Attempting to take this machine offline: " + node.name);
+def computer = node.getComputer()
+
+if (node.getComputer().isOffline()) {
+    println("  Offline, skipping");
+}
+else {
     computer.setTemporarilyOffline(true);
-    if (!computer.isIdle()) {
-        println("  Not idle after offline, skipping!");
-        computer.setTemporarilyOffline(false);
-        continue;
-    }
-  }
-  else {
-    println("  Not Idle");
-  }
 }
