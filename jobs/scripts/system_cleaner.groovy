3e34c87876abcb563d0396be7db537c083117a62
Matt Mitchell <mmitche@microsoft.com>
2016-09-02 10:10:22 -0700

    System cleaner can now delete empty folders

--- jobs/scripts/system_cleaner.groovy
+++ jobs/scripts/system_cleaner.groovy
@@ -2,20 +2,38 @@ import hudson.model.*
 import jenkins.model.*
 
 
-deleteDisabledNonRootChildren(Jenkins.instance.items, '')
+deleteDisabledNonRootChildren(Jenkins.instance.items)
 
-def deleteDisabledNonRootChildren(items, folder) {
+def deleteDisabledNonRootChildren(items) {
+    def removedAllItems = true
     for (item in items) {
         if (item.class.canonicalName != 'com.cloudbees.hudson.plugins.folder.Folder') {
-            if (item.class.canonicalName != 'org.jenkinsci.plugins.workflow.job.WorkflowJob') {
-                // Workflow jobs don't derive from AbstractProject, so cannot be disabled
+            if (item.class.canonicalName == 'org.jenkinsci.plugins.workflow.job.WorkflowJob') {
+                // Workflow jobs don't derive from AbstractProject, so cannot be disabled.  In this case, print the potential
+                // orphaned item and continue
+                println (item.fullName + " cannot be deleted or disabled (is workflow job)!")
+                continue
             }
             if (item.disabled) {
-                println("About to delete " + folder + "/" + item.name)
+                println("About to delete " + item.fullName)
                 item.delete();
+            } else {
+                removedAllItems = false
             }
         } else {
-            deleteDisabledNonRootChildren(((com.cloudbees.hudson.plugins.folder.Folder) item).getItems(), folder + "/" + item.name)
+            def removedAllItemsFromFolder = 
+                deleteDisabledNonRootChildren(((com.cloudbees.hudson.plugins.folder.Folder) item).getItems())
+            if (removedAllItemsFromFolder) {
+                println("About to delete " + item.fullName)
+                // Delete the folder too
+                item.delete()
+            }
+            else {
+                // We didn't remove everything from the folder, so make sure removeAllItems is updated
+                removedAllItems = false
+            }
         }
     }
+    
+    return removedAllItems
 }
