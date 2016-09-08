6324a9fe58cd330e593a3deec26c8783506cf80c
Matt Mitchell <mmitche@microsoft.com>
2016-09-08 13:36:04 -0700

    Add pre and post generation
    Run two additional groovy scripts.  PreGen.groovy goes before the netci.groovy execution, while PostGen.groovy goes after.  Since each groovy execution within the same DSL step shares the same state, Pre and Post gen can affect/be affected by the netci.grooy execution.

---
+++ jobs/generation/PostGen.groovy
@@ -0,0 +1,4 @@
+// This file is invoked after the repo generator file is run
+// This file can be used to run post steps, like creating job reports or help jobs
+
+println("Running PostGen")
---
+++ jobs/generation/PreGen.groovy
@@ -0,0 +1,9 @@
+// This file is invoked before the repo generator file is run.
+// Within the same DSL step, the groovy files will see the same object model.
+// So code in this file can affect the code in the netci.groovy.  For instance,
+// this file could set a global indicating this is a pr test generation, which could
+// be used to to alter any number of options (for instance, altering triggers so that
+// generated jobs can be left un-disabled, but the default trigger options won't cause the
+// job to run for everyone.
+
+println("Running PreGen")
