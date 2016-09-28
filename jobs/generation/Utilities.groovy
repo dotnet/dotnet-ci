abad01182a2986d4fe2b53293fc2aa77e4ddfd98
Tanner Gooding <tannergooding@users.noreply.github.com>
2016-09-28 09:10:48 -0700

    Adding an internal image for VS15.P5 (#476)

--- jobs/generation/Utilities.groovy
+++ jobs/generation/Utilities.groovy
@@ -199,6 +199,8 @@ class Utilities {
                                 'latest-dev15':'auto-win2012-20160506',
                                 // For internal runs
                                 'latest-or-auto-internal':'windows-internal || auto-win2012-20160707-internal',
+                                // For internal runs - Win2012.R2 + VS15.P5
+                                'latest-or-auto-dev15-internal':'auto-win2012-20160927-internal',
                                 // For internal runs which don't need/want the static 'windows-internal' pool
                                 'latest-dev15-internal':'auto-win2012-20160707-internal',
                                 // For elevated runs
