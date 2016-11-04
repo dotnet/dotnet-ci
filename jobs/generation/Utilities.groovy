406ed139697a07ce0613c27706b06b1a9aa36b7d
Matt Mitchell <mmitche@microsoft.com>
2016-11-04 11:59:12 -0700

    Add missing comma

--- jobs/generation/Utilities.groovy
+++ jobs/generation/Utilities.groovy
@@ -235,7 +235,7 @@ class Utilities {
                                 ],
                             'Windows 7' :
                                 [
-                                '20161104','win2008-20161104'
+                                '20161104','win2008-20161104',
                                 // Latest auto image.
                                 'latest-or-auto':'win2008-20161104'
                                 ],
