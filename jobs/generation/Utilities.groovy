4708e263006716bdf85190cc4cd626473f758fdf
Matt Mitchell <mmitche@microsoft.com>
2017-04-06 13:32:36 -0700

    Allow restriction to non-DTAP machines (#651)
    Allow restriction of generation to non-dtap.  No machines are currently labeled DTAP.

--- jobs/generation/Utilities.groovy
+++ jobs/generation/Utilities.groovy
@@ -367,7 +367,7 @@ class Utilities {
                             'Generators' :
                                 [
                                 '' : '!windowsnano16',
-                                'latest-or-auto':'!windowsnano16 && !performance'
+                                'latest-or-auto':'!windowsnano16 && !performance && !dtap'
                                 ]
                             ]
         def versionLabelMap = machineMap.get(osName, null)
