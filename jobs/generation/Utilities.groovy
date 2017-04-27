aa3a8833c0829db1cc4df030a0c94a8d8d525d48
Matt Mitchell <mmitche@microsoft.com>
2017-04-27 16:07:52 -0700

    Add image to utils

--- jobs/generation/Utilities.groovy
+++ jobs/generation/Utilities.groovy
@@ -245,6 +245,8 @@ class Utilities {
                                 'latest-or-auto-dev15-rc':'win2016-20170214',
                                 // Win2016 + VS15.0
                                 'latest-or-auto-dev15-0':'win2016-20170307',
+                                // For internal runs - Win2016 + VS15.1
+                                'latest-dev15-1':'win2016-20170427',
                                 // Dev15 image
                                 'latest-dev15':'auto-win2012-20160506',
                                 // For internal runs
@@ -255,6 +257,8 @@ class Utilities {
                                 'latest-or-auto-dev15-internal':'win2016-20170214-internal',
                                 // For internal runs - Win2016 + VS15.0
                                 'latest-or-auto-dev15-0-internal':'win2016-20170307-internal',
+                                // For internal runs - Win2016 + VS15.1
+                                'latest-dev15-1-internal':'win2016-20170427-internal',
                                 // For internal runs which don't need/want the static 'windows-internal' pool
                                 'latest-dev15-internal':'auto-win2012-20160707-internal',
                                 // For elevated runs
