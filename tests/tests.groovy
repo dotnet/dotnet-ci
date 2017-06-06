c8b580c8f4850950d6feefb8c23baee8ffa2bcd8
Matt Mitchell <mmitche@microsoft.com>
2017-06-06 09:59:10 -0700

    Add raw tests.groovy

---
+++ tests/tests.groovy
@@ -0,0 +1,24 @@
+// Import the utility functionality.
+
+import jobs.generation.JobReport;
+import jobs.generation.Utilities;
+import org.dotnet.ci.pipelines.Pipeline
+
+// Expected VSTS or GitHub SCM
+assert binding.variables.get("VersionControlLocation") != null && 
+       (binding.variables.get("VersionControlLocation") == 'VSTS' || 
+       binding.variables.get("VersionControlLocation") != 'GitHub') : "Expected what version control this server targets (VSTS or GitHub)"
+       
+// The input project name (e.g. dotnet/corefx)
+def qualifiedRepoName = QualifiedRepoName
+// The input branch name (e.g. master)
+def targetBranchName = TargetBranchName
+
+// This file is pretty barebones.  It basically just sets up a pipeline which then calls the tests.
+// This is opposed to setting up all the tests here, which is a little more difficult to square between PR and non-PR scenarios,
+// as well as for verification purposes.
+def testingPipeline = Pipeline.createPipeline(this, VersionControlLocation, project, branch, 'tests/root-tests-pipeline.groovy')
+
+// Trigger this pipeline on pushes and PRs
+testingPipeline.triggerPipelineOnEveryPR('CI Tests')
+testingPipeline.triggerPipelineOnPush()
