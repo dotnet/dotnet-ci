d64d8a1825bc735bc7428bf7d989e5759dc87411
Matt Mitchell <mmitche@microsoft.com>
2017-06-06 15:50:50 -0700

    First attempt at adding a DSL test

--- tests/root-tests-pipeline.groovy
+++ tests/root-tests-pipeline.groovy
@@ -5,3 +5,14 @@ echo "Incoming parameters: "
 echo "    OrgOrProjectName = ${OrgOrProjectName}"
 echo "    RepoName = ${RepoName}"
 echo "    GitBranchOrCommit = ${GitBranchOrCommit}"
+
+// DSL tests
+node {
+    stage ('DSL Generation Tests') {
+        // Run DSL
+        jobDsl targets: 'tests/dsl-new-pipeline.groovy',
+           removedJobAction: 'DELETE',
+           removedViewAction: 'DELETE',
+           lookupStrategy: 'SEED_JOB'
+    }
+}
