92e747b0c1cf384d5ee2a4ac997e852cc31e5312
Matt Mitchell <mmitche@microsoft.com>
2017-06-06 15:34:22 -0700

    Add initial tests pipeline

---
+++ tests/root-tests-pipeline.groovy
@@ -0,0 +1,7 @@
+// Entering
+
+echo "Running tests for CI SDK"
+echo "Incoming parameters: "
+echo "    OrgOrProjectName = ${OrgOrProjectName}"
+echo "    RepoName = ${RepoName}"
+echo "    GitBranchOrCommit = ${GitBranchOrCommit}"
