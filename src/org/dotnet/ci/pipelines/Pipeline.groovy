b398c814f603dfed7ce7e57eb7b21fd43de3cde0
Matt Mitchell <mmitche@microsoft.com>
2017-06-07 12:33:49 -0700

    Asserts can't be caught

--- src/org/dotnet/ci/pipelines/Pipeline.groovy
+++ src/org/dotnet/ci/pipelines/Pipeline.groovy
@@ -204,7 +204,7 @@ class Pipeline {
     public def triggerPipelineOnEveryPR(String context, Map<String,Object> parameters = [:]) {
         if (this._scm.getScmType() == 'VSTS') {
             // TODO: VSTS PR checks
-            assert false : "VSTS PR trigger NYI"
+            return null
         }
         else if (this._scm.getScmType() == 'GitHub') {
             return triggerPipelineOnEveryGithubPR(context, parameters)
--- tests/dsl/pipeline_pr_trigger.groovy
+++ tests/dsl/pipeline_pr_trigger.groovy
@@ -3,9 +3,4 @@ import org.dotnet.ci.pipelines.Pipeline
 // Tests creation of a pipeline that triggers on pushes with non-string parameter types
 def newPipeline = Pipeline.createPipeline(this, 'simple_pr.groovy')
 
-try {
-    newPipeline.triggerPipelineOnEveryPR('CI Test', ['Hello':false])
-}
-catch (e) {
-    assert e.getMessage().indexOf('VSTS PR trigger NYI') != -1 : "Expected VSTS PR trigger was NYI"
-}
+assert newPipeline.triggerPipelineOnEveryPR('CI Test', ['Hello':false]) == null : "Expected VSTS PR trigger was NYI"
