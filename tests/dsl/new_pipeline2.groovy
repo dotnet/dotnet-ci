13f4cf70d2d1c3e7256bd12a2557c75da889d515
Matt Mitchell <mmitche@microsoft.com>
2017-06-07 12:18:35 -0700

    Add try/catch construct to DSL

--- tests/dsl/new_pipeline2.groovy
+++ tests/dsl/new_pipeline2.groovy
@@ -1,11 +1,13 @@
 import org.dotnet.ci.pipelines.Pipeline
 import jobs.generation.GenerationSettings
 
-// Set for SDK testing
-GenerationSettings.setSDKTest()
-
 // Tests creation of a pipeline that triggers on pushes with non-string parameter types
 def newPipeline = Pipeline.createPipeline(this, 'foopipeline.groovy')
 
-// Parameters
-newPipeline.triggerPipelineOnPush(['Hello':false])
+try {
+    // Parameters
+    newPipeline.triggerPipelineOnPush(['Hello':false])
+}
+catch (e) {
+    //???
+}
