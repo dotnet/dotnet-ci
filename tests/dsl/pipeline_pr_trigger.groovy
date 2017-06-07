851170817ad3a48c4d73a20c655724bbddb68e8b
Matt Mitchell <mmitche@microsoft.com>
2017-06-07 12:29:51 -0700

    Change name of target pipelines

--- tests/dsl/pipeline_pr_trigger.groovy
+++ tests/dsl/pipeline_pr_trigger.groovy
@@ -1,6 +1,6 @@
 import org.dotnet.ci.pipelines.Pipeline
 
 // Tests creation of a pipeline that triggers on pushes with non-string parameter types
-def newPipeline = Pipeline.createPipeline(this, 'foopipeline.groovy')
+def newPipeline = Pipeline.createPipeline(this, 'simple_pr.groovy')
 
 newPipeline.triggerPipelineOnEveryPR('CI Test', ['Hello':false])
--- tests/dsl/pipeline_simple_push_trigger.groovy
+++ tests/dsl/pipeline_simple_push_trigger.groovy
@@ -1,7 +1,7 @@
 import org.dotnet.ci.pipelines.Pipeline
 
 // Tests creation of a pipeline that triggers on pushes
-def newPipeline = Pipeline.createPipeline(this, 'foopipeline.groovy')
+def newPipeline = Pipeline.createPipeline(this, 'simple_push.groovy')
 
 // No parameters
 newPipeline.triggerPipelineOnPush()
