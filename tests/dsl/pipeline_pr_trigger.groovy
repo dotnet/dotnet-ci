c9670ac262433831a55fee14e67aa6c7019bc608
Matt Mitchell <mmitche@microsoft.com>
2017-06-07 12:32:29 -0700

    Make VSTS PR trigger NYI

--- tests/dsl/pipeline_pr_trigger.groovy
+++ tests/dsl/pipeline_pr_trigger.groovy
@@ -3,4 +3,9 @@ import org.dotnet.ci.pipelines.Pipeline
 // Tests creation of a pipeline that triggers on pushes with non-string parameter types
 def newPipeline = Pipeline.createPipeline(this, 'simple_pr.groovy')
 
-newPipeline.triggerPipelineOnEveryPR('CI Test', ['Hello':false])
+try {
+    newPipeline.triggerPipelineOnEveryPR('CI Test', ['Hello':false])
+}
+catch (e) {
+    assert e.getMessage().indexOf('VSTS PR trigger NYI') != -1 : "Expected VSTS PR trigger was NYI"
+}
