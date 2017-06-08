26b530fb3f6370a6c961da7c28e707a3a7a90f0e
Matt Mitchell <mmitche@microsoft.com>
2017-06-08 09:38:14 -0700

    Attempt to parameterize library import Add test for getBranch

--- tests/pipeline/pipeline-tests.groovy
+++ tests/pipeline/pipeline-tests.groovy
@@ -6,9 +6,30 @@ echo "    OrgOrProjectName = ${OrgOrProjectName}"
 echo "    RepoName = ${RepoName}"
 echo "    GitBranchOrCommit = ${GitBranchOrCommit}"
 
-// DSL tests
+// Remove the */ from GitBranchOrCommit, the import the library
+String libraryImportBranch = GitBranchOrCommit
+if (GitBranchOrCommit.indexOf('*/') == 0) {
+    libraryImportBranch = GitBranchOrCommit.substring(2)
+}
+
+@Library("dotnet-ci@${libraryImportBranch}") _
+
+// Basic tests
+// Currently in one file, but could be in multiple
+
+// Simple source control checkout (should always pass, built in functionality)
 node {
     stage ('Source Control Checkout') {
         checkout scm
+    }
+}
+
+// getBranch
+node {
+    stage ('getBranch - Test 1') {
+        checkout scm
+
+        String branch = getBranch()
+        assert branch == libraryImportBranch : "Expected getBranch would return ${libraryImportBranch}"
     }
 }
