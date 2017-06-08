1ff9e8aae5157180929bc4f56a27cadb58e9182d
Matt Mitchell <mmitche@microsoft.com>
2017-06-08 15:25:11 -0700

    Figure out where the notseriazable is coming from

--- vars/getCommitForDir.groovy
+++ vars/getCommitForDir.groovy
@@ -7,13 +7,13 @@
 def call() {
     if (isUnix()) {
         // Prefix with @ to avoid the command in the output
-        def output = sh script: '@git rev-parse HEAD', returnStdout: true
+        def output = sh script: 'git rev-parse HEAD', returnStdout: true
         echo output
         return output
     }
     else {
         // Prefix with @ to avoid the command in the output
-        def output = bat script: '@git rev-parse HEAD', returnStdout: true
+        def output = bat script: 'git rev-parse HEAD', returnStdout: true
         echo output
         return output
     }
