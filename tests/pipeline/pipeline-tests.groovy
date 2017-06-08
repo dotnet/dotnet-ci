5036915747789f37ef926b3b56a2bf2672353698
Matt Mitchell <mmitche@microsoft.com>
2017-06-08 15:05:03 -0700

    Set overall test timeout at 2 hours Reset timeout for node allocation at 60 minutes

--- tests/pipeline/pipeline-tests.groovy
+++ tests/pipeline/pipeline-tests.groovy
@@ -19,69 +19,72 @@ stage ('Check out target library') {
 }
 
 stage ('Run Tests') {
-    // Basic tests
-    // Currently in one file, but could be in multiple.  Run in parallel by default
+    // Overall test timeout at 2 hours
+    timeout (120) {
+        // Basic tests
+        // Currently in one file, but could be in multiple.  Run in parallel by default
 
-    parallel (
-        // This is an array in the form of:
-        // "test name" : { // test code }
+        parallel (
+            // This is an array in the form of:
+            // "test name" : { // test code }
 
-        "Raw Node Test" : {
-            // Simple source control checkout (should always pass, built in functionality)
-            node {
-                checkout scm
-            }
-        },
+            "Raw Node Test" : {
+                // Simple source control checkout (should always pass, built in functionality)
+                node {
+                    checkout scm
+                }
+            },
 
-        // Test that simple nodes work, of various types
-        "simpleNode - Windows_NT - latest" : {
-            timeout (5) {
-                simpleNode('Windows_NT', 'latest') { }
-            }
-        },
+            // Test that simple nodes work, of various types
+            "simpleNode - Windows_NT - latest" : {
+                timeout (60) {
+                    simpleNode('Windows_NT', 'latest') { }
+                }
+            },
 
-        "simpleNode - Ubuntu14.04 - latest" : {
-            timeout (5) {
-                simpleNode('Ubuntu14.04', 'latest') { }
-            }
-        },
+            "simpleNode - Ubuntu14.04 - latest" : {
+                timeout (60) {
+                    simpleNode('Ubuntu14.04', 'latest') { }
+                }
+            },
 
-        "getBranch" : {
-            // getBranch
-            node {
+            "getBranch" : {
+                // getBranch
+                node {
+                    simpleNode('Windows_NT', 'latest') {
+                        checkout scm
+
+                        echo "Checking that getBranch returns ${libraryImportBranch}"
+                        String branch = getBranch()
+                        assert branch == libraryImportBranch : "Expected getBranch would return ${libraryImportBranch}"
+                    }
+                }
+            },
+
+            // getCommit, varies on unix and windows, so test both
+            "getCommit - Windows systems" : {
                 simpleNode('Windows_NT', 'latest') {
                     checkout scm
 
-                    echo "Checking that getBranch returns ${libraryImportBranch}"
-                    String branch = getBranch()
-                    assert branch == libraryImportBranch : "Expected getBranch would return ${libraryImportBranch}"
+                    echo "Checking that getCommit returns valid commit on NT based system"
+                    String commit = getCommit
+                    echo "Got ${commit}"
+                    // Check that it's probably a valid hash
+                    assert commit.length() == 40 : "Commit doesn't look like a valid hash'"
                 }
-            }
-        },
+            },
 
-        // getCommit, varies on unix and windows, so test both
-        "getCommit - Windows systems" : {
-            simpleNode('Windows_NT', 'latest') {
-                checkout scm
+            "getCommit - Unix systems" : {
+                simpleNode('Ubuntu14.04', 'latest') {
+                    checkout scm
 
-                echo "Checking that getCommit returns valid commit on NT based system"
-                String commit = getCommit
-                echo "Got ${commit}"
-                // Check that it's probably a valid hash
-                assert commit.length == 40 : "Commit doesn't look like a valid hash'"
-            }
-        },
-
-        "getCommit - Unix systems" : {
-            simpleNode('Ubuntu14.04', 'latest') {
-                checkout scm
-
-                echo "Checking that getCommit returns valid commit on NT based system"
-                String commit = getCommit
-                echo "Got ${commit}"
-                // Check that it's probably a valid hash
-                assert commit.length == 40 : "Commit doesn't look like a valid hash'"
-            }
-        },
-    )
+                    echo "Checking that getCommit returns valid commit on NT based system"
+                    String commit = getCommit
+                    echo "Got ${commit}"
+                    // Check that it's probably a valid hash
+                    assert commit.length() == 40 : "Commit doesn't look like a valid hash'"
+                }
+            },
+        )
+    }
 }
