eff1a12babc5d2d6b6549c6a4ab72a48102f65e5
Matt Mitchell <mmitche@microsoft.com>
2017-06-12 12:46:23 -0700

    Fix library import during PR test

--- tests/pipeline/pipeline-tests.groovy
+++ tests/pipeline/pipeline-tests.groovy
@@ -1,12 +1,29 @@
 // To figure out what we import, we need to know whether this is a PR or not.
-// If a PR, and GitHub, we grab the source branch, which must live in the same repo
-// Remove the */ from GitBranchOrCommit, the import the library
-String libraryImportBranch = GitBranchOrCommit
-if (GitBranchOrCommit.indexOf('*/') == 0) {
-    libraryImportBranch = GitBranchOrCommit.substring(2)
+// If a PR, and GitHub, we grab the source branch, which must live in the same repo.
+// Note that we can't use the library isPR for this.
+boolean isPRTest = false
+String repository = env["ghprbGhRepository"]
+String libraryImportBranch
+if (repository != null && repository != "") {
+    echo "This is a GitHub PR test"
+    // Check that the PR source branch came from the correct location (otherwise the tests are expected to fail)
+    if (repository != 'dotnet/dotnet-ci') {
+         error "PRs tests of functionality that changes the CI pipeline SDK are only valid for branches pushed to dotnet/dotnet-ci.  If you need that testing please push your branch to dotnet-ci.  Otherwise ignore this failure"
+    }
+
+    libraryImportBranch = env["ghprbSourceBranch"]
+    assert libraryImportBranch != null && libraryImportBranch != '' : "Library branch (ghprbSourceBranch) was unexpectedly empty"
+}
+else {
+    echo "This is a VSTS PR test or commit test"
+    // TODO: VSTS PR support
+    libraryImportBranch = GitBranchOrCommit
+    if (libraryImportBranch.indexOf('*/') == 0) {
+        libraryImportBranch = libraryImportBranch.substring(2)
+    }
 }
 
-def libraryName = "github.com/mmitche/dotnet-ci@${ghprbSourceBranch}"
+def libraryName = "dotnet-ci/${libraryImportBranch}"
 
 stage ('Check out target library') {
     echo "Checking out ${libraryName}"
