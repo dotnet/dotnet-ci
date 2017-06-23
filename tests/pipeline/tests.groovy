52d86bba31f3b5a9ad41baf8dc3be4d5ed391575
Matt Mitchell <mmitche@microsoft.com>
2017-06-23 11:21:15 -0700

    Add help job (mostly for testing) (#721)
    * Add help job (mostly for testing)

--- tests/pipeline/tests.groovy
+++ tests/pipeline/tests.groovy
@@ -1,4 +1,5 @@
 import org.dotnet.ci.pipelines.Pipeline
+import jobs.generation.Utilities
 
 // This file is pretty barebones.  It basically just sets up a pipeline which then calls the tests.
 // This is opposed to setting up all the tests here, which is a little more difficult to square between PR and non-PR scenarios,
@@ -11,3 +12,8 @@ if (VersionControlLocation != 'VSTS') {
     testingPipeline.triggerPipelineOnEveryPR('CI Tests')
 }
 testingPipeline.triggerPipelineOnPush()
+
+// Make the call to generate the help job
+Utilities.createHelperJob(this, QualifiedRepoName, TargetBranchName,
+    "Welcome to the ${QualifiedRepoName} Repository",  // This is prepended to the help message
+    "Have a nice day!")  // This is appended to the help message.  You might put known issues here.
