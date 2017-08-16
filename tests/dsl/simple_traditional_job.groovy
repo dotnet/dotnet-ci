430f8cc809e83f0e49b209de9ee6b7b47dd9ae5c
Matt Mitchell <mmitche@microsoft.com>
2017-08-16 09:53:34 -0700

    Add test for new params

--- tests/dsl/simple_traditional_job.groovy
+++ tests/dsl/simple_traditional_job.groovy
@@ -5,16 +5,18 @@ import jobs.generation.Utilities;
 def project = QualifiedRepoName
 def branch = TargetBranchName
 
-def newJob = job(Utilities.getFullJobName(project, os, isPR)) {
-    steps {
-        if (os == 'Windows_NT') {
-            batchFile('''call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat" && build.cmd''')
-        }
-        else {
-            shell('''./build.sh''')
+[true, false].each { isPR ->
+    def newJob = job(Utilities.getFullJobName(project, "FooBarTest", isPR)) {
+        steps {
+            if (os == 'Windows_NT') {
+                batchFile('''call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat" && build.cmd''')
+            }
+            else {
+                shell('''./build.sh''')
+            }
         }
     }
-}
 
-Utilities.setMachineAffinity(newJob, os, 'latest-or-auto')
-Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+    Utilities.setMachineAffinity(newJob, os, 'latest-or-auto')
+    Utilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+}
