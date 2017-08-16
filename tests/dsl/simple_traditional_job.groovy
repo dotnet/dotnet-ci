e0ef407684c15c1a94b2fd74a1c1aee87cd7d20e
Matt Mitchell <mmitche@microsoft.com>
2017-08-16 09:54:55 -0700

    Add test for new params

--- tests/dsl/simple_traditional_job.groovy
+++ tests/dsl/simple_traditional_job.groovy
@@ -8,12 +8,7 @@ def branch = TargetBranchName
 [true, false].each { isPR ->
     def newJob = job(Utilities.getFullJobName(project, "FooBarTest", isPR)) {
         steps {
-            if (os == 'Windows_NT') {
-                batchFile('''call "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat" && build.cmd''')
-            }
-            else {
-                shell('''./build.sh''')
-            }
+            batchFile("echo Hello World")
         }
     }
 
