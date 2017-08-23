98989186b7604cadcbc357fd0c8c996f12b9252f
Christopher Costa <chcosta@microsoft.com>
2017-08-23 16:45:53 -0700

    change format

--- vars/getBuildNumber.groovy
+++ vars/getBuildNumber.groovy
@@ -2,5 +2,5 @@
   * Retrieves formatted official build number.
   */
 def call() {
-    return "${BUILD_YEAR}${BUILD_MONTH, XX}${BUILD_DAY, XX}-${BUILDS_TODAY, XX}"
+    return '${BUILD_YEAR}${BUILD_MONTH, "XX"}${BUILD_DAY, "XX"}-${BUILDS_TODAY, "XX"}''
 }
