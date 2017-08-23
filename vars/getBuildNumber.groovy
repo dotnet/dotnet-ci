5f006e1ff286de3a5ccd2deafcedb03fded50a3d
Christopher Costa <chcosta@microsoft.com>
2017-08-23 16:56:07 -0700

    build num testing

--- vars/getBuildNumber.groovy
+++ vars/getBuildNumber.groovy
@@ -2,9 +2,9 @@
   * Retrieves formatted official build number.
   */
 def call() {
-    println ("${BUILD_YEAR}")
-    println ("${BUILD_MONTH}")
-    println ("${BUILD_DAY}")
-    println ("${BUILDS_TODAY}")
-    return "${BUILD_YEAR}"
+    println (BUILD_YEAR)
+    println (BUILD_MONTH)
+    println (BUILD_DAY)
+    println (BUILDS_TODAY)
+    return "BUILD_YEAR"
 }
