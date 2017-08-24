eb326c20732f8331321491a222203c024c5c3597
Christopher Costa <chcosta@microsoft.com>
2017-08-23 17:13:51 -0700

    another try

--- vars/getBuildNumber.groovy
+++ vars/getBuildNumber.groovy
@@ -1,18 +1,7 @@
-import org.jvnet.hudson.tools.versionnumber
 /**
   * Retrieves formatted official build number.
   */
 def call() {
-    VersionNumberBuilder versionNumberBuilder = new VersionNumberBuilder(
-      '${BUILD_YEAR}${BUILD_MONTH, "XX"}${BUILD_DAY, "XX"}-${BUILDS_TODAY, "XX"}', 
-      null,
-      "BUILDNUMBER",
-      null,
-      null,
-      null,
-      null,
-      null,
-      null,
-      false)
-    return versionNumberBuilder.getVersionNumberString()
+    def versionNumber = new VersionNumber('${BUILD_YEAR}${BUILD_MONTH, "XX"}${BUILD_DAY, "XX"}-${BUILDS_TODAY, "XX"}')
+    return versionNumber
 }
