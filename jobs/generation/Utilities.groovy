96c229f5a83f109ab861e4eae1505e1149f7ba74
Matt Mitchell <mmitche@microsoft.com>
2017-08-29 15:23:01 -0700

    Remove EOL'd OS's
    FreeBSD should be added back with a version suffix on the same.

--- jobs/generation/Utilities.groovy
+++ jobs/generation/Utilities.groovy
@@ -331,11 +331,6 @@ class Utilities {
                                 // Latest auto image.
                                 'latest-or-auto':'win2008-20170303'
                                 ],
-                            'FreeBSD' :
-                                [
-                                // Latest auto image.
-                                'latest-or-auto':'freebsd-20161026'
-                                ],
                             'RHEL7.2' :
                                 [
                                 '' : 'auto-rhel72-20170525',
@@ -352,14 +347,6 @@ class Utilities {
                                 'outer-latest-or-auto':'centos71-20170216-outer',
                                 // For outerloop runs, using Linux kernel version 4.6.4
                                 'outer-linux464': 'auto-auto-centos71-20160609.1-20160715outer'
-                                ],
-                            'OpenSUSE13.2' :
-                                [
-                                '' : 'auto-suse132-20160315',
-                                // Latest auto image.
-                                'latest-or-auto':'auto-suse132-20160315',
-                                // For outerloop runs
-                                'outer-latest-or-auto':'auto-suse132-20160315outer'
                                 ],
                             'OpenSUSE42.1' :
                                 [
@@ -381,14 +368,6 @@ class Utilities {
                                 'latest-or-auto':'deb84-20170214',
                                 // For outerloop runs
                                 'outer-latest-or-auto':'deb84-20170214-outer'
-                                ],
-                           'Fedora23' :
-                                [
-                                '' : 'auto-fedora23-20160622',
-                                // Latest auto image.
-                                'latest-or-auto':'auto-fedora23-20160622',
-                                // For outerloop runs
-                                'outer-latest-or-auto':'auto-fedora23-20160622outer'
                                 ],
                             'Fedora24' :
                                 [
