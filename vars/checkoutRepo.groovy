a63a26bc6603371c3961b81d0b335bc2abb84f10
Matt Mitchell <mmitche@microsoft.com>
2017-09-01 15:51:08 -0700

    Change back to using retry

--- vars/checkoutRepo.groovy
+++ vars/checkoutRepo.groovy
@@ -3,17 +3,7 @@
   * Includes automatic retry.  The normal retry {} is problematic at least in some pipeline versions
   */
 def call() {
-    int checkoutTries = 10
-    while (checkoutTries > 0) {
-        checkoutTries--
-        try {
-            checkout scm
-        } catch (e) {
-            if (checkoutTries > 0) {
-                echo "Failed to checkout, retrying. (${checkoutTries} attempts remaining)"
-            } else {
-                echo "Failed to checkout, Aborting. No attempts remaining"
-            }
-        }
+    retry (10) {
+        checkout scm
     }
 }
