c3cad3953f543699ec35cb537a45881ef0801348
Matt Mitchell <mmitche@microsoft.com>
2017-09-08 14:56:26 -0700

    Add other missing server

--- management/nodes/Clean-Resource-Group.ps1
+++ management/nodes/Clean-Resource-Group.ps1
@@ -11,7 +11,7 @@ param (
     [Parameter(Mandatory=$true)]
     [string]$ResourceGroupName,
     [switch]$RunForever = $false,
-    $Servers = @("https://ci.dot.net","https://ci2.dot.net","https://ci3.dot.net"),
+    $Servers = @("https://ci.dot.net","https://ci2.dot.net","https://ci3.dot.net","https://dotnet-ci4.westus2.cloudapp.azure.com"),
     [string]$Regex = '.*[a-f0-9]{6}$',
     [switch]$DryRun = $false
 )
