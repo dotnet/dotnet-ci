beb71cf1cd2417cdc56855a96fd940d00630f222
Matt Mitchell <mmitche@microsoft.com>
2017-09-26 09:04:15 -0700

    Re-enable notifications

--- src/jobs/generation/Utilities.groovy
+++ src/jobs/generation/Utilities.groovy
@@ -339,8 +339,7 @@ class Utilities {
         Utilities.addRetentionPolicy(job, isPR)
         // Add a webhook to gather job events for Jenkins monitoring.
         // The event hook is the id of the event hook URL in the Jenkins store
-        // TEMPORARILY DISABLED TO REDUCE LOG OUTPUT
-        // Utilities.setBuildEventWebHooks(job, ['helix-int-notification-url', 'helix-prod-notification-url', 'legacy-notification-url'])
+        Utilities.setBuildEventWebHooks(job, ['helix-int-notification-url', 'helix-prod-notification-url', 'legacy-notification-url'])
     }
 
     def private static String joinStrings(Iterable<String> strings, String combineDelim) {
