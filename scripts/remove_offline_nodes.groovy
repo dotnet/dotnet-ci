df7cbfd96ccc234fbc4c1c685385396aa5a58f72
Matt Mitchell <mmitche@microsoft.com>
2017-11-15 08:28:53 -0800

    Add script that removes offline azure nodes not taken manually offline This works around a current bug that sometimes leaves an agent in Jenkins without being cleaned up if deletion from azure fails.  This bug is fixed in a currently unreleased Azure VM Agents version.

---
+++ scripts/remove_offline_nodes.groovy
@@ -0,0 +1,21 @@
+// Removes offline nodes from AzureVMAgents that weren't taken offline for user reasons
+
+import com.microsoft.azure.vmagent.AzureVMAgent 
+
+def nodes = Jenkins.instance.getNodes()
+
+nodes.each { node ->
+  if (node instanceof AzureVMAgent) {
+    if (node.getComputer() != null) {
+      if (node.getComputer().isTemporarilyOffline()) {
+        def cause = node.getComputer().getOfflineCause()
+        if (!(cause instanceof hudson.slaves.OfflineCause.UserCause)) {
+          println "Removing " + node.getComputer().getName() + " "
+          Jenkins.instance.removeNode(node)
+        }
+      }
+    }
+  }
+}
+
+return true
