45220c20f7835eebeb49ca13753f5bb5955bfb59
Matt Mitchell <mmitche@microsoft.com>
2018-03-28 09:26:31 -0700

    Remove Helix agents in node cleanup script (#970)
    Sometimes Jenkins takes agents offline for no apparent reasnon.  We remove these agents periodically.  Add Helix agents to the list of agents to remove

--- scripts/remove_offline_nodes.groovy
+++ scripts/remove_offline_nodes.groovy
@@ -1,16 +1,18 @@
 // Removes offline nodes from AzureVMAgents that weren't taken offline for user reasons
 
 import jenkins.model.Jenkins
-import com.microsoft.azure.vmagent.AzureVMAgent 
+import com.microsoft.azure.vmagent.AzureVMAgent
+import com.microsoft.helix.helixagents.HelixAgent
 
 def nodes = Jenkins.instance.getNodes()
 
 nodes.each { node ->
-  if (node instanceof AzureVMAgent) {
+  if (node instanceof AzureVMAgent || node instanceof HelixAgent) {
     if (node.getComputer() != null) {
       if (node.getComputer().isTemporarilyOffline()) {
         def cause = node.getComputer().getOfflineCause()
-        if (!(cause instanceof hudson.slaves.OfflineCause.UserCause)) {
+        if (!(cause instanceof hudson.slaves.OfflineCause.UserCause) && 
+            (!(node instanceof HelixAgent) || !(cause instanceof hudson.slaves.OfflineCause.SimpleOfflineCause))) {
           println "Removing " + node.getComputer().getName() + " "
           Jenkins.instance.removeNode(node)
         }
